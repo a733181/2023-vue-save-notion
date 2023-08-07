ad3a9c79da2c9c130262a605db58b4f3d3647b20
a733181 <87145013+a733181@users.noreply.github.com>
2023-08-07 12:59:45 +0000

    Deploying to gh-pages from @ a733181/2023-vue-save-notion@828fa11ee35387a6116f98cc0567fdd1d90053d2 🚀

---
+++ assets/youtube-ab7f5e8f.js
@@ -0,0 +1,1 @@
+import{h as s,i as r}from"./index-bc04b16e.js";const n=s("useYoutubeStore",()=>{async function o(e){try{const{data:t}=await r.post("/youtubelist",e);return!!t.success}catch{return!1}}async function u(e){try{const{data:t}=await r.post("/youtubevideo",e);return!!t.success}catch{return!1}}return{youtubeListToNotionHandler:o,youtubeVideoToNotionHandler:u}});export{n as u};
